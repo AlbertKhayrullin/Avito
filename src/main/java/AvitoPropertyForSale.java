b2243ca1b253ac46d46833c85c7285cd0a5f3f19
AlbertKhayrullin <albertKh1620@rambler.ru>
2021-03-03 19:54:13 +0300

    src added

---
+++ src/main/java/AvitoPropertyForSale.java
@@ -0,0 +1,25 @@
+public class AvitoPropertyForSale {
+    int id;
+    String URL; //фото
+    int room;
+    String roomComment;  //  -к. квартира,
+    double apartmentArea;
+    String areaComment;   // м2,
+    int floor;
+    String floorBetween;   //    17 / 19
+    int floorTotal;
+    String floorComment;  // эт.
+    Boolean favorite;
+    int price;
+    String priceComment;   //  Рубли
+    boolean verified;      // проверено ЕГРН
+    String residentialComplex;
+    String address;
+    String metroStation;
+    double distanceToMetro;
+    String distanceComment;     // км.
+    String agencyName;
+    String URL1;                // логотип агенства недвижимости
+    int placementTime;
+    String placementTimeComment;   //  минут дней назад
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        AvitoPropertyForSale offer = new AvitoPropertyForSale();
+        offer.room = 1;
+        offer.roomComment = "-к. квартира, ";
+        offer.apartmentArea = 31.2;
+        offer.areaComment = " м2, ";
+        offer.floor = 9;
+        offer.floorBetween = "/";
+        offer.floorTotal = 19;
+        offer.floorComment = "эт.";
+        System.out.print(offer.room);
+        System.out.print(offer.roomComment);
+        System.out.print(offer.apartmentArea);
+        System.out.print(offer.areaComment);
+        System.out.print(offer.floor);
+        System.out.print(offer.floorBetween);
+        System.out.println(offer.floorTotal);
+        System.out.println(offer.floorComment);
+    }
+}
